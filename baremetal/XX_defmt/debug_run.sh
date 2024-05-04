e79af34e75fc997777880ac6cc08cfdf70218e8e
Toshifumi NISHINAGA <tnishinaga.dev@gmail.com>
2024-05-04 06:59:21 +0000

    use openocd instead of probe-rs

--- baremetal/XX_defmt/debug_run.sh
+++ baremetal/XX_defmt/debug_run.sh
@@ -2,6 +2,10 @@
 
 PROGRAM=$1
 
+openocd -f interface/cmsis-dap.cfg -f ../bcm2712.cfg &
+OPENOCD_PID=$!
 # rust-gdb -q -x gdb_init "$PROGRAM"
 gdb-multiarch -q -x gdb_init "$PROGRAM"
+
+kill -TERM ${OPENOCD_PID}
 
